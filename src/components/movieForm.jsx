e7233be5337408266244694fe6f3d0917cb79732
AbasovAbbas <abbas_abasov@list.ru>
2021-06-12 07:38:46 -0700

    commit 3

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function MovieForm(props) {
     return (
         <div>
+            <p>Best Movie</p>
             <h1>Movie From {props.match.params.id}</h1>
             <button onClick = {() => props.history.push('/movies')} className="btn btn-danger">Save</button>
         </div>
